b2b16e7ea47338581c3928a97abfd0fcb8f50fa3
llrs <lluis.revilla@gmail.com>
2018-07-27 15:52:46 +0200

    Export functions

--- R/own_algorithm.R
+++ R/own_algorithm.R
@@ -23,6 +23,7 @@ entropy <- function(x){
 #' @param omit Name of the columns of the pheno that will be omitted
 #' @param iterations Numeric value of iterations that will be performed
 #' @return The indices of which samples go with which batch
+#' @export
 design <- function(pheno, size.batch, omit, iterations = 500) {
   opt <- Inf
 
@@ -67,6 +68,7 @@ design <- function(pheno, size.batch, omit, iterations = 500) {
 #' @param n A numeric value with the number of batchs
 #' @param size.data A numeric value of the amount of samples to distribute
 #' @return A random list of indices of the samples
+#' @export
 #' @examples
 #' create_subset(50, 2, 100)
 create_subset <- function(size.subset, n, size.data) {
@@ -91,6 +93,7 @@ create_subset <- function(size.subset, n, size.data) {
 #' @param i List of indices
 #' @param pheno Data.frame with the samples
 #' @return A Matrix with the sd value for each column
+#' @export
 evaluate_sd <- function(i, pheno){
   stopifnot(sum(lengths(i))== nrow(pheno))
   # Distribution of sd
@@ -104,6 +107,7 @@ evaluate_sd <- function(i, pheno){
 #' @param i List of indices
 #' @param pheno Data.frame with the samples
 #' @return A Matrix with the mean value for each column
+#' @export
 evaluate_mean <- function(i, pheno) {
   stopifnot(sum(lengths(i))== nrow(pheno))
   # Calculates the distribution
@@ -118,6 +122,7 @@ evaluate_mean <- function(i, pheno) {
 #' Given an index return the name of the batches the samples are in
 #' @param i A list of numeric indices of the data
 #' @return A character vector with the names of the batch for each sample
+#' @export
 batch_names <- function(i) {
   xy <- seq_along(i)
   isub <- lapply(xy, function(x) {
@@ -154,6 +159,7 @@ evaluate_helper <- function(x, original_x){
 #' @param i list of numeric indices of the data.frame
 #' @param pheno Data.frame
 #' @return Value to minimize
+#' @export
 evaluate_num <- function(i, pheno) {
 
   # Compare with the optimum (the original distribution)
@@ -182,6 +188,7 @@ evaluate_num <- function(i, pheno) {
 #' @param i list of numeric indices of the data.frame
 #' @param pheno Data.frame
 #' @return Value to minimize
+#' @export
 evaluate_cat <- function(i, pheno) {
   num <- vapply(pheno, is.numeric, logical(1L))
   # Calculate the entropy for the categorical values
