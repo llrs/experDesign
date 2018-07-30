799ff6291a50173db764295e2c12a5b0bd33115e
llrs <lluis.revilla@gmail.com>
2018-07-30 15:03:23 +0200

    Allow to optimize without omitting anything and just numeric or just categoric type

--- R/designer.R
+++ R/designer.R
@@ -9,41 +9,59 @@
 #' @param iterations Numeric value of iterations that will be performed
 #' @return The indices of which samples go with which batch
 #' @export
-design <- function(pheno, size.batch, omit, iterations = 500) {
+design <- function(pheno, size.batch, omit = NULL, iterations = 500) {
   opt <- Inf
 
   # Calculate batches
   batches <- ceiling(nrow(pheno)/size.batch)
 
   # Omit columns
-  pheno_o <- pheno[, !colnames(pheno) %in% omit, drop = FALSE]
-
+  if (!is.null(omit)){
+    pheno_o <- pheno[, !colnames(pheno) %in% omit, drop = FALSE]
+  } else {
+    pheno_o <- pheno
+  }
   # Find the numeric values
   num <- vapply(pheno_o, is.numeric, logical(1L))
 
-  # Compare with the optimum (the original distribution)
-  original_n <- vapply(pheno_o[, num, drop = FALSE], mean, numeric(1L), na.rm = TRUE)
-  original_n_sd <- vapply(pheno_o[, num, drop = FALSE], sd, numeric(1L), na.rm = TRUE)
+  thereis_numbers <- sum(num) >= 1
+  thereis_category <- sum(!num) >= 0
+  if (thereis_numbers) {
+    # Compare with the optimum (the original distribution)
+    original_n <- vapply(pheno_o[, num, drop = FALSE], mean, numeric(1L), na.rm = TRUE)
+    original_n_sd <- vapply(pheno_o[, num, drop = FALSE], sd, numeric(1L), na.rm = TRUE)
+  } else if (thereis_category) {
+    # Calculate the entropy for the categorical values
+    original_e <- vapply(droplevels(pheno_o[, !num]), entropy, numeric(1L))
+    # Remove those that are different in each sample (Hopefully just the name of the sample)
+    remove_e <- original_e == 1
+  }
 
-  # Calculate the entropy for the categorical values
-  original_e <- vapply(droplevels(pheno_o[, !num]), entropy, numeric(1L))
-  # Remove those that are different in each sample (Hopefully just the name of the sample)
-  remove_e <- original_e == 1
-
-  message()
   for (x in seq_len(iterations)) {
     i <- create_subset(size.batch, batches, nrow(pheno_o))
-    opt_e <- .evaluate_cat(i, pheno_o[, !num, drop = FALSE], remove_e)
-    opt_n <- .evaluate_num(i, pheno_o[, num, drop = FALSE], original_n, original_n_sd)
-    opt_i <- evaluate_independence(i, pheno_o[, !num, drop = FALSE])
+    if (thereis_category) {
+      opt_e <- .evaluate_cat(i, pheno_o[, !num, drop = FALSE], remove_e)
+      opt_i <- evaluate_independence(i, pheno_o[, !num, drop = FALSE])
+    } else if (thereis_numbers) {
+      opt_n <- .evaluate_num(i, pheno_o[, num, drop = FALSE], original_n, original_n_sd)
+    }
 
     # Minimize the value
-    opt_o <- abs(sum(opt_n, opt_e, opt_i, na.rm = TRUE))
+    if (thereis_numbers & thereis_category){
+      opt_o <- abs(sum(opt_n, opt_e, opt_i, na.rm = TRUE))
+    } else if (thereis_category) {
+      opt_o <- abs(sum(opt_e, opt_i, na.rm = TRUE))
+    } else if (thereis_numbers) {
+      opt_o <- opt_n
+    }
+
+    # store index if "better"
     if (opt_o <= opt){
       opt <- opt_o
-      val <- i # store in a different site
+      val <- i
     }
   }
+  message("Minimum value reached of ", round(opt))
   val
 }
 
