b3b3f8953134433c91ec258cd83ed6df40dae9cd
llrs <lluis.revilla@gmail.com>
2018-07-31 17:32:51 +0200

    Consider the case where there are categories and numeric values

--- R/designer.R
+++ R/designer.R
@@ -39,7 +39,8 @@ design <- function(pheno, size.batch, omit = NULL, iterations = 500) {
     # Compare with the optimum (the original distribution)
     original_n <- vapply(pheno_o[, num, drop = FALSE], mean, numeric(1L), na.rm = TRUE)
     original_n_sd <- vapply(pheno_o[, num, drop = FALSE], sd, numeric(1L), na.rm = TRUE)
-  } else if (thereis_category) {
+  }
+  if (thereis_category) {
     # Calculate the entropy for the categorical values
     original_e <- vapply(droplevels(pheno_o[, !num]), entropy, numeric(1L))
     # Remove those that are different in each sample (Hopefully just the name of the sample)
@@ -52,7 +53,8 @@ design <- function(pheno, size.batch, omit = NULL, iterations = 500) {
     if (thereis_category) {
       opt_e <- .evaluate_cat(i, pheno_o[, !num, drop = FALSE], remove_e)
       opt_i <- evaluate_independence(i, pheno_o[, !num, drop = FALSE])
-    } else if (thereis_numbers) {
+    }
+    if (thereis_numbers) {
       opt_n <- .evaluate_num(i, pheno_o[, num, drop = FALSE], original_n, original_n_sd)
     }
 
