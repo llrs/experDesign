cf773f2e9cc9d2935d6c275b513e55f683703302
llrs <lluis.revilla@gmail.com>
2018-08-01 13:16:21 +0200

    Rewritte to take into account the new fast functions

--- R/designer.R
+++ R/designer.R
@@ -23,55 +23,42 @@ design <- function(pheno, size.batch, omit = NULL, iterations = 500) {
     pheno_o <- pheno
   }
 
-  mean_nas <- vapply(pheno_o, function(x){mean(is.na(x))}, numeric(1L))
+  original_pheno <- evaluate_orig(pheno_o)
+  original_pheno["na", ] <- original_pheno["na", ]/batches
 
   # Find the numeric values
-  num <- vapply(pheno_o, is.numeric, logical(1L))
   dates <- vapply(pheno_o, function(x){methods::is(x, "Date")}, logical(1L))
   if (any(dates)) {
     warning("The dates will be treat as categories")
   }
 
-  thereis_numbers <- sum(num) >= 1
-  thereis_category <- sum(!num) >= 0
-
-  if (thereis_numbers) {
-    # Compare with the optimum (the original distribution)
-    original_n <- vapply(pheno_o[, num, drop = FALSE], mean, numeric(1L), na.rm = TRUE)
-    original_n_sd <- vapply(pheno_o[, num, drop = FALSE], sd, numeric(1L), na.rm = TRUE)
-  }
-  if (thereis_category) {
-    # Calculate the entropy for the categorical values
-    original_e <- vapply(droplevels(pheno_o[, !num]), entropy, numeric(1L))
-    # Remove those that are different in each sample (Hopefully just the name of the sample)
-    remove_e <- original_e == 1
-  }
+  num <- vapply(pheno, is.numeric, logical(1L))
+  # Numbers are evaluated 4 times, and categories only 2
+  eval_n <- ifelse(num, 4, 3)
 
   for (x in seq_len(iterations)) {
     i <- create_subset(size.batch, batches, nrow(pheno_o))
 
-    if (thereis_category) {
-      opt_e <- .evaluate_cat(i, pheno_o[, !num, drop = FALSE], remove_e)
-      opt_i <- evaluate_independence(i, pheno_o[, !num, drop = FALSE])
-    }
-    if (thereis_numbers) {
-      opt_n <- .evaluate_num(i, pheno_o[, num, drop = FALSE], original_n, original_n_sd)
-    }
+    subsets <- evaluate_index(i, pheno_o)
+    # Evaluate the differences between the subsets and the originals
+    differences <- abs(sweep(subsets, c(1, 2), original_pheno))
+    # Add the independence of the categorical values
+    subset_ind <- evaluate_independence(i, pheno_o)
 
-    opt_nas <- .evaluate_na(i, pheno_o, mean_nas)
+    # Calculate the score for each subset by variable
+    meanDiff <- apply(differences, 3, function(x) {
+
+      x <- rbind(x, "ind" = 0)
+      x <- insert(x, subset_ind, name = "ind")
+      colSums(x, na.rm = TRUE)/eval_n
+    })
 
     # Minimize the value
-    if (thereis_numbers & thereis_category){
-      opt_o <- sum(opt_nas, opt_n, opt_e, opt_i, na.rm = TRUE)
-    } else if (thereis_category) {
-      opt_o <- sum(opt_nas, opt_e, opt_i, na.rm = TRUE)
-    } else if (thereis_numbers) {
-      opt_o <- sum(opt_nas, opt_n)
-    }
+    optimize <- sum(rowMeans(meanDiff))
 
     # store index if "better"
-    if (opt_o <= opt){
-      opt <- opt_o
+    if (optimize <= opt){
+      opt <- optimize
       val <- i
     }
   }
