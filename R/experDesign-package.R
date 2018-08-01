cfeb3f6add7968fedc79c06825e8d2052311e6a3
llrs <lluis.revilla@gmail.com>
2018-08-01 16:10:11 +0200

    Add documentation about the package

---
+++ R/experDesign-package.R
@@ -0,0 +1,15 @@
+#' experDesign: Expert experiment design in batches
+#'
+#' Enables easy distribution of samples per batch avoiding batch and
+#' counfounding effects by randomization of the variables in each batch.
+#'
+#' The most important function is \code{\link{design}}, which distributes
+#' samples in batches according to the information provided.
+#'
+#' To help in the bench there is the \code{\link{inspect}} function that appends
+#' the group to the data provided.
+#' @author Lluís Revilla
+#' @name experDesign-package
+#' @aliases experDesign
+#' @docType package
+NULL
