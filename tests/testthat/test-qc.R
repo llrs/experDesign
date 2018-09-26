b2bb99ef4740f2ddf1befa49a7b76e3d1936d764
llrs <lluis.revilla@gmail.com>
2018-09-26 11:55:22 +0200

    Test qcSubset

---
+++ tests/testthat/test-qc.R
@@ -0,0 +1,14 @@
+context("qcSubset")
+
+test_that("all", {
+  index <- create_subset(50, 2, 100)
+  QC_samples <- qcSubset(index, 10)
+  expect_length(QC_samples, 10)
+})
+
+test_that("by batch", {
+  index <- create_subset(50, 2, 100)
+  QC_samples <- qcSubset(index, 10, TRUE)
+  expect_length(QC_samples, 2)
+  expect_equal(lengths(QC_samples), c("SubSet1" = 10L, "SubSet2" = 10L))
+})
