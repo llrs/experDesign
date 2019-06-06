88c99bf5111d7818fd93cc4dda11766b3b55c9db
llrs <lluis.revilla@gmail.com>
2019-06-06 15:28:35 +0200

    Correct the test

--- tests/testthat/test-extreme_cases.R
+++ tests/testthat/test-extreme_cases.R
@@ -3,11 +3,7 @@ context("test-extreme_cases")
 test_that("extreme_cases works", {
   set.seed(456)
   surv <- MASS::survey
-  size <- optimum_size(surv)
-  expect_message(
-    expect_warning(samples <- extreme_cases(surv, size = size),
-                 "Maximum number of iterations reached"),
-    "Maximum value reached:")
+  samples <- extreme_cases(surv, size = 119)
   expect_length(samples, 119L)
 })
 
