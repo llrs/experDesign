e6f12e89644be1b8dc6a66e961851f1bbd34c518
llrs <lluis.revilla@gmail.com>
2019-06-06 11:14:26 +0200

    Test the relation of use_index and batch_names

--- tests/testthat/test_batch-names.R
+++ tests/testthat/test_batch-names.R
@@ -4,4 +4,8 @@ test_that("works", {
   index <- create_subset(100, 50, 2)
   batch <- batch_names(index)
   expect_length(unique(batch), 2)
+
+
+  plates <- c("P1", "P2", "P1", "P2", "P2", "P3", "P1", "P3", "P1", "P1")
+  expect_equal(batch_names(use_index(plates)), plates)
 })
