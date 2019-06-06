f72c7315596cfe024d7b73161ea28f368aba1657
llrs <lluis.revilla@gmail.com>
2019-06-06 11:15:41 +0200

    A function to calculate the sizes of the batches

--- R/indexing.R
+++ R/indexing.R
@@ -32,19 +32,18 @@ create_subset <- function(size_data, size_subset = NULL, n = NULL) {
 .create_index <- function(size_data, size_subset, n) {
 
   names_batches <- paste0("SubSet", seq_len(n))
-  min_subset <- floor(size_data/n)
+  # The size of each batch
+  size_batches <- sizes_batches(size_data, size_subset, n)
 
   # Create the subsets
   i <- vector("list", length = n)
+  names(i) <- names_batches
   vec <- seq_len(size_data)
   for (j in seq_len(n)) {
-    lv <- length(vec)
-    s <- ifelse(size_subset*j <= lv, size_subset, min(min_subset, lv))
-    out <- sample(vec, size = s)
+    out <- sample(vec, size = size_batches[j])
     vec <- vec[!vec %in% out]
     i[[j]] <- out
   }
-  names(i) <- names_batches
   i
 }
 
@@ -54,7 +53,7 @@ create_subset <- function(size_data, size_subset = NULL, n = NULL) {
 #' @param x A character or a factor to be used as index
 #' @export
 #' @seealso  You can use \code{\link{evaluate_index}} to evaluate how good an
-#' index is
+#' index is. For the inverse look at  \code{\link{batch_names}}
 #' @examples
 #' plates <- c("P1", "P2", "P1", "P2", "P2", "P3", "P1", "P3", "P1", "P1")
 #' use_index(plates)
@@ -68,12 +67,31 @@ use_index <- function(x){
 #' Given an index return the name of the batches the samples are in
 #' @param i A list of numeric indices.
 #' @return A character vector with the names of the batch for each the index.
-#' @seealso \code{\link{create_subset}}
+#' @seealso \code{\link{create_subset}}, for the inverse look at
+#' \code{\link{use_index}}
 #' @export
 #' @examples
-#' index <- create_subset(50, 2, 100)
+#' index <- create_subset(100, 50, 2)
 #' batch <- batch_names(index)
 #' head(batch)
 batch_names <- function(i) {
-  rep(names(i), lengths(i))
+  names <- rep(names(i), lengths(i))
+  names[order(unlist(i, use.names = FALSE))]
+}
+
+# Return the size of each batch so that there is less problems
+sizes_batches <- function(size_data, size_subset, batches) {
+  # Look for all combination of sizes for that number of batches
+  pos <- seq(from = 0, to = batches)
+  factors <- t(expand.grid(pos, pos))
+  factors <- factors[, colSums(factors) == batches, drop = FALSE]
+
+  # Look for those that get equal to size_data
+  out <- t(factors) %*% c(size_subset, size_subset-1)
+  pos <- which(out[, 1] == size_data)[1] # To get the first one
+  # If the size cannot be equal to the data return the original size_subset
+  if (is.na(pos)) {
+    return(size_subset)
+  }
+  rep(c(size_subset, size_subset-1), factors[, pos])
 }
