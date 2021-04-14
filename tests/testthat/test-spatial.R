23bbac3dd7214d4fa0fbe2d0203228712a489d31
llrs <lluis.revilla@gmail.com>
2021-04-14 09:57:38 +0200

    Correct test to new size

--- tests/testthat/test-spatial.R
+++ tests/testthat/test-spatial.R
@@ -6,9 +6,9 @@ test_that("spatial works", {
   index2 <- spatial(index, survey[, c("Sex", "Smoke", "Age")],
                     rows = LETTERS[1:9],
                     columns = 1:12, iterations = 25)
-  expect_length(index2, 50)
-  expect_equivalent(table(lengths(index2)), as.table(c("4" = 13, "5" = 37)))
-  expect_equal(names(index2), position_name(rows = LETTERS[1:5], 1:10)$name)
+  expect_length(index2, 9*12)
+  expect_equivalent(table(lengths(index2)), as.table(c("2" = 87, "3" = 21)))
+  expect_equal(names(index2), position_name(rows = LETTERS[1:9], 1:12)$name)
 })
 
 test_that("spatial works with replicates", {
