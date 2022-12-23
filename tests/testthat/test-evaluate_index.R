ecf5cbf2fcd96c98dd06141126a87389cd321139
Lluís Revilla Sancho <lrevilla@irsicaixa.es>
2022-12-23 15:50:17 +0100

    Avoid warning

--- tests/testthat/test-evaluate_index.R
+++ tests/testthat/test-evaluate_index.R
@@ -1,11 +1,9 @@
-context("evaluate_index")
-
-
 test_that("works" ,{
   set.seed(43)
-  samples <- data.frame(L = letters[1:25], Age = rnorm(25))
+  samples <- data.frame(L = letters[1:25], Age = rnorm(25),
+                        type = sample(LETTERS[1:5], 25, TRUE))
   i <- create_subset(25, 5, 5)
-  arr <- evaluate_index(i, samples)
+  arr <- evaluate_index(i, samples[, -1])
   expect_equal(arr["entropy", "Age",], c("SubSet1" = 0,
                                          "SubSet2" = 0,
                                          "SubSet3" = 0,
