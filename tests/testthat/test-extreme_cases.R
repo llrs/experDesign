7f43b66843f36282f59e7d23d4eba75ee77ea30d
Lluís Revilla Sancho <lrevilla@irsicaixa.es>
2023-01-30 17:11:23 +0100

    Make tests faster

--- tests/testthat/test-extreme_cases.R
+++ tests/testthat/test-extreme_cases.R
@@ -1,7 +1,7 @@
 test_that("extreme_cases works", {
   set.seed(456)
   surv <- MASS::survey
-  samples <- extreme_cases(surv, size = 119)
+  samples <- extreme_cases(surv, size = 119, iterations = 10)
   expect_length(samples, 119L)
 })
 
@@ -13,7 +13,8 @@ test_that("check_index works", {
   out <- check_index(surv[-nas, c("Sex", "Smoke", "Age")], index)
   expect_equal(dim(out), c(4L, 2L))
 
-  i2 <- design(surv[-nas, c("Sex", "Smoke", "Age")], size_subset = 119, iterations = 100)
+  i2 <- design(surv[-nas, c("Sex", "Smoke", "Age")],
+               size_subset = 119, iterations = 100)
   o2 <- check_index(surv[-nas, c("Sex", "Smoke", "Age")], i2)
   expect_true(sum(out) >= sum(o2))
 })
