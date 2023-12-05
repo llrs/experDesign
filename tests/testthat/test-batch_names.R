f09e9a15e655b45c145ada0d44448c8409b073de
Lluís <lluis.revilla@gmail.com>
2023-12-05 22:02:12 +0100

    Explicitly test that the index matches the names

--- tests/testthat/test-batch_names.R
+++ tests/testthat/test-batch_names.R
@@ -2,6 +2,8 @@ test_that("batch_names works", {
   index <- create_subset(100, 50, 2)
   batch <- batch_names(index)
   expect_length(unique(batch), 2)
+  expect_true(all(batch[index[[1]]] == names(index)[1]))
+  expect_true(all(batch[index[[2]]] == names(index)[2]))
 
 
   plates <- c("P1", "P2", "P1", "P2", "P2", "P3", "P1", "P3", "P1", "P1")
